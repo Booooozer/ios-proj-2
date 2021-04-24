523c9602a73677ddbb01094cf5348f14615ac8a1
Booooozer <xmatus37@vutbr.cz>
2021-04-24 02:28:25 +0200

    Initial commit, created struct for input parameters

---
+++ proj2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef struct {
+    int numElves;
+    int numRaind;
+    int timeElf;
+    int timeRaind;
+}params;
+
+int main(int argc, char *argv[]) {
+    if (argc != 5) {
+        fprintf(stderr, "Invalid input parameters\n");
+        return 1;
+    }
+
+
+
+
+    return 0;
+}
